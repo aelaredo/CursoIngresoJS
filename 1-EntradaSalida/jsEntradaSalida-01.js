67eaae665f19600ba32d08aac5391961ce33cc07
aelaredo <aelaredo@users.noreply.github.com>
2020-07-10 16:22:45 -0300

    prueba 10 julio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,6 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
-{
-	alert("ok");
+function mostrar() {
+    alert("prueba 10 kjulio");
 }
-
