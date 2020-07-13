2726dd3bc43e696234865d96938d8236610aeecf
aelaredo <aelaredo@users.noreply.github.com>
2020-07-13 19:38:13 -0300

    Clase 13 julio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,5 +2,5 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar() {
-    alert("Chau Mundo xdxd");
+    alert("clase 13 julio");
 }
