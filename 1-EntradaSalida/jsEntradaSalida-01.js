a6aa5553cc9968333f0b15bc228a0bf34b1755d4
aelaredo <62897484+aelaredo@users.noreply.github.com>
2020-07-13 19:12:45 -0300

    Add files via upload
    Chau mundo en jsentradasalida1.js

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,5 +2,5 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar() {
-    alert("prueba 10 kjulio");
+    alert("Chau Mundo xdxd");
 }
